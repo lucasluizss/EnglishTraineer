84bafd70f3a2a2519c9fe3ef8fec610686f5ccab
Lucas <lucasluizss@live.com>
2021-05-22 12:43:16 +0100

    Adding settings

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,11 @@ if (environment.production) {
 
 document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if (environment.production && 'serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistration()
+        .then(active => !active && navigator.serviceWorker.register('/EnglishTrainner/ngsw-worker.js'));
+    }
+  })
   .catch(err => console.error(err));
 });
