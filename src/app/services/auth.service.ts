653b7a42d1ccc751b477003d3aee6c4471d9e1b1
Lucas <lucasluizss@live.com>
2021-05-22 19:28:20 +0100

    adding auth service methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  public user = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('@EnglishTrainner:user')));
 
-  constructor() { }
-  // this.isUser = auth.authState.pipe(map(authState => (authState ? authState.uid : null)));
+  constructor(private readonly fireBaseAuth: AngularFireAuth) {}
+
+  async signIn({ email, password}): Promise<void> {
+    const  { user } = await this.fireBaseAuth.signInWithEmailAndPassword(email, password);
+    this.user.next(user);
+    localStorage.setItem('@EnglishTrainner:user', JSON.stringify(user));
+  }
+
+  async signUp({ email, password}): Promise<void> {
+    const  { user } = await this.fireBaseAuth.createUserWithEmailAndPassword(email, password);
+    this.user.next(user);
+    localStorage.setItem('@EnglishTrainner:user', JSON.stringify(user));
+  }
+
+  logout(): void {
+    this.fireBaseAuth.signOut();
+    this.user.next(null);
+    localStorage.clear();
+  }
 }
